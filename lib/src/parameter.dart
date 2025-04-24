26b0f7ae52e312e8d3c861c7bef7b348434a1157
RobertNeher <Robert.Neher@gmail.com>
2025-04-24 07:58:52 +0200

    Font stuff removed from settings

--- lib/src/parameter.dart
+++ lib/src/parameter.dart
@@ -2,8 +2,6 @@ class Parameter {
   int windowSize = 0;
   double centerX = 0;
   double centerY = 0;
-  String font = "";
-  int fontSize = 0;
   double astronomicalUnit = 0;
   double gravityConstant = 0;
   double scaleFactor = 0;
@@ -15,8 +13,6 @@ class Parameter {
     required this.windowSize,
     required this.centerX,
     required this.centerY,
-    required this.font,
-    required this.fontSize,
     required this.astronomicalUnit,
     required this.gravityConstant,
     required this.scaleFactor,
@@ -30,8 +26,6 @@ class Parameter {
       windowSize: json['windowSize'],
       centerX: json['centerX'].toDouble(),
       centerY: json['centerY'].toDouble(),
-      font: json['font'],
-      fontSize: json['fontSize'],
       astronomicalUnit: json['astronomicalUnit'].toDouble(),
       gravityConstant: json['gravityConstant'].toDouble(),
       scaleFactor: double.parse(json['scaleFactor']),
@@ -48,8 +42,6 @@ class Parameter {
     print("Window size: ${this.windowSize}");
     print("Gravity Constant: ${this.gravityConstant}");
     print("centerX/Y: ${this.centerX}/${this.centerY}");
-    print("Font: ${this.font}(${this.fontSize})");
-    print("Scale: ${this.scaleFactor}");
     print("Total size factor: ${this.totalSizeFactor}");
     print("Timestep: ${this.timeStep}");
     print("Period: ${this.updatePeriod}");
